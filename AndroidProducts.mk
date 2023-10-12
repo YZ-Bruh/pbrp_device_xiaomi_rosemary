1341fc8f72fd35ce3730bc2ca834e28bcc25f1b5
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-10-12 22:02:33 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,5 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_rosemary.mk
-
-COMMON_LUNCH_CHOICES := \
-    twrp_rosemary-user \
-    twrp_rosemary-userdebug \
-    twrp_rosemary-eng
+# 
+  
+ PRODUCT_MAKEFILES := \ 
+      $(LOCAL_DIR)/twrp_rosemary.mk 
+ 
